278f63a8be96c8b97e1cab5ff39cb54521face7b
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-29 20:44:34 +0200

    Modify application path names from posts to recipe

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       provider: "v8",
       include: ["**/*.tsx", "**/*.ts"],
       exclude: ["main.tsx", "**/*.d.ts", "**/types.ts", "**/App.tsx"],
-      reportsDirectory: "../coverage",
+      reportsDirectory: "coverage",
     },
   },
 });
