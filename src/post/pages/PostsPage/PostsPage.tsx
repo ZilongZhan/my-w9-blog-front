9fe9a156cda5e616f08ca73524f88474bef33fc9
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-02 20:43:40 +0200

    Integrate post deletion logic to PostCard delete button

--- src/post/pages/PostsPage/PostsPage.tsx
+++ src/post/pages/PostsPage/PostsPage.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router";
-import usePosts from "../../hooks/usePosts";
 import PostsList from "../../components/PostsList/PostsList";
 import "./PostsPage.css";
 import Paginator from "../../components/Paginator/Paginator";
+import usePostsProvider from "../../hooks/usePostsProvider";
 
 const PostsPage: React.FC = () => {
-  const { posts, postsTotal, loadPostsInfo } = usePosts();
+  const { posts, postsTotal, loadPostsInfo } = usePostsProvider();
   const [page] = useSearchParams();
 
   const pageNumber = Number(page.get("page")) || 1;
